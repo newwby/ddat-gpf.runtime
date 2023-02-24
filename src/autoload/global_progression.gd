57951c995b0d50d2e6986e2c9e141ceaa6658b86
Dan <60653155+newwby@users.noreply.github.com>
2023-02-24 22:00:37 +0000

    automatic playtime tracker

--- src/autoload/global_progression.gd
+++ src/autoload/global_progression.gd
@@ -19,6 +19,14 @@ const SCRIPT_NAME := "GlobalProgression"
 # for developer use, enable if making changes
 # verbose_logging exists in a parent class
 #const VERBOSE_LOGGING := true
+
+# if set, and the GameProgressFile has the property 'total_playtime', the
+# globalProgression singleton will auto-instantiate a timer node and use it
+# to update the second count value of the total playtime property.
+const TRACK_TOTAL_PLAY_TIME := true
+
+var total_play_time_tracker: Timer
+
 #
 #08. exported variables
 #09. public variables
@@ -29,12 +37,70 @@ var all_game_files := []
 # the active save file loaded
 # should be unloaded when game meta transitions out
 # warning-ignore:unused_class_variable
-var loaded_save_file: GameProgressFile
+var loaded_save_file: GameProgressFile setget _set_loaded_save_file
 
 #10. private variables
 #11. onready variables
 
 ##############################################################################
+
+# setget
+
+# start the playtime tracker if it has been instantiated
+func _set_loaded_save_file(arg_value):
+	loaded_save_file = arg_value
+	if total_play_time_tracker != null:
+		total_play_time_tracker.stop()
+		if loaded_save_file != null:
+			total_play_time_tracker.start()
+
+
+##############################################################################
+
+# virt
+
+func _ready():
+	_initiate_playtime_tracker()
+
+
+##############################################################################
+
+# public
+
+
+#//UNFIN, REMOVE
+#//TODO move this to a global function (core?)
+#func check_property_in_class(arg_class, arg_property: String):
+#	if arg_class.has_met
+
+
+##############################################################################
+
+# private
+
+
+func _increment_playtime_tracker():
+	if loaded_save_file != null:
+#		print("test1")
+		loaded_save_file.total_playtime += 1
+#		loaded_save_file.total_pl(1)
+#		print(loaded_save_file.total_playtime)
+#		print(loaded_save_file.save_file_element_info)
+
+
+func _initiate_playtime_tracker():
+	var class_to_check = GameProgressFile.new()
+	var property_to_check = "total_playtime"
+	if property_to_check in class_to_check\
+	and TRACK_TOTAL_PLAY_TIME:
+		total_play_time_tracker = Timer.new()
+		self.call_deferred("add_child", total_play_time_tracker)
+		total_play_time_tracker.autostart = false
+		total_play_time_tracker.one_shot = false
+		if total_play_time_tracker.connect(\
+				"timeout", self, "_increment_playtime_tracker") != OK:
+			GlobalDebug.log_error(SCRIPT_NAME, "_initiate_playtime_tracker",
+					"total playtime tracker not setup correctly")
 
 
 # shadowing a parent class method
