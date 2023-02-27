d553bcf44fd80bb322136630c3e6fca1c2d969cb
Dan <60653155+newwby@users.noreply.github.com>
2023-02-27 09:18:31 +0000

    deprecate _create_new_save_resource

--- src/runtime/state_game_meta/game_file_load_dialog/game_meta_load_dialog.gd
+++ src/runtime/state_game_meta/game_file_load_dialog/game_meta_load_dialog.gd
@@ -43,34 +43,36 @@ onready var new_save_file_button_node =\
 # private methods)
 
 
-func _create_new_save_resource(arg_new_save: GameProgressFile):
-	#//TODO add get date created (and add date modified to resaving)
-	#TODO remove this test line
-	arg_new_save.total_playtime = 50
-	
-	var save_id := 1
-	var does_save_id_exist := true
-	var save_path =\
-			GlobalData.DATA_PATHS[GlobalData.DATA_PATH_PREFIXES.GAME_SAVE]
-	while does_save_id_exist == true:
-		does_save_id_exist =\
-				GlobalData.validate_file(
-					save_path+"save"+str(save_id)+".tres"
-				)
-		if does_save_id_exist == true:
-			save_id += 1
-	
-	if does_save_id_exist == false:
-		if GlobalData.save_resource(
-			save_path,
-			"save"+str(save_id)+".tres",
-			arg_new_save
-		) == OK:
-			print("new save recorded at {path}".format({
-				"path": save_path+"save"+str(save_id)+".tres"
-			}))
-		assert(typeof(GlobalProgression.all_game_files) == TYPE_ARRAY)
-		GlobalProgression.all_game_files.append(arg_new_save)
+#//DERPECATED as functionality is replicated in globalProgression
+#//(see GlobalProgression.create_game_file)
+#func _create_new_save_resource(arg_new_save: GameProgressFile):
+#	#//TODO add get date created (and add date modified to resaving)
+#	#TODO remove this test line
+#	arg_new_save.total_playtime = 50
+#
+#	var save_id := 1
+#	var does_save_id_exist := true
+#	var save_path =\
+#			GlobalData.DATA_PATHS[GlobalData.DATA_PATH_PREFIXES.GAME_SAVE]
+#	while does_save_id_exist == true:
+#		does_save_id_exist =\
+#				GlobalData.validate_file(
+#					save_path+"save"+str(save_id)+".tres"
+#				)
+#		if does_save_id_exist == true:
+#			save_id += 1
+#
+#	if does_save_id_exist == false:
+#		if GlobalData.save_resource(
+#			save_path,
+#			"save"+str(save_id)+".tres",
+#			arg_new_save
+#		) == OK:
+#			print("new save recorded at {path}".format({
+#				"path": save_path+"save"+str(save_id)+".tres"
+#			}))
+#		assert(typeof(GlobalProgression.all_game_files) == TYPE_ARRAY)
+#		GlobalProgression.all_game_files.append(arg_new_save)
 
 
 # arg_save_file is the GameProgressFile used to load the player's save state
